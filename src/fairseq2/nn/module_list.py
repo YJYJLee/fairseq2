671779d5dea0ca45e399b72153809bf40568bd4f
Mostafa Elhoushi <melhoushi@meta.com>
2024-07-08 22:36:48 +0000

    use setter wrapper for drop_p

--- src/fairseq2/nn/module_list.py
+++ src/fairseq2/nn/module_list.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Iterable, Iterator, Optional, final
+from typing import Iterable, Iterator, List, Optional, Union, final
 
 import torch
 from torch.nn import Module
@@ -39,10 +39,10 @@ class ModuleList(ModuleListBase):
     ...    x = layer(x)
     """
 
-    drop_p: float
+    _drop_p: List[float]
 
     def __init__(
-        self, modules: Optional[Iterable[Module]] = None, *, drop_p: float = 0.0
+        self, modules: Optional[Iterable[Module]] = None, *, drop_p: Union[float, Iterable[float]] = 0.0
     ) -> None:
         """
         :param modules:
@@ -52,7 +52,7 @@ class ModuleList(ModuleListBase):
         """
         super().__init__(modules)
 
-        self.drop_p = [drop_p] * len(modules)
+        self.drop_p = drop_p
 
     def drop_iter(self) -> Iterator[Module]:
         """Return an iterator that drops a random set of submodules."""
@@ -73,3 +73,19 @@ class ModuleList(ModuleListBase):
             s = f"{s}, drop_p={self.drop_p}"
 
         return s
+
+    @property
+    def drop_p(self):
+        """Get probability of dropping each layer."""
+        return self._drop_p
+
+    @drop_p.setter
+    def drop_p(self, drop_p: Union[float, Iterable[float]]):
+        """Set probability of dropping layers using either a single value or a list of values."""
+        if isinstance(drop_p, Iterable):
+            assert len(drop_p) == len(self)
+            self._drop_p = drop_p
+        elif isinstance(drop_p, float):
+            self._drop_p = [drop_p] * len(self)
+        else:
+            raise ValueError(f"Unsupported type for drop rate {drop_p}. Expecting either float or list of floats.")
